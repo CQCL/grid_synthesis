7e4e37b132be0e8bb0d4f12f3a096aa1d7962ec1
Nihar <nihar.gargava@quantinuum.com>
2022-09-30 17:03:32 +0100

    Nihar has commited

---
+++ src/algorithms/exact_synth.rs
@@ -0,0 +1,6 @@
+// This is the exact synthesis algorithm
+//
+// The point of this code is to implement step 3 in the outline
+// given on Section 7.3 of arxiv:1403.2975v3
+//
+// This would imply implementing the exact synthesis from arxiv:1206.5236
