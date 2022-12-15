367b3a37a827171803f009952d2e297c042b607a
nihargargava <nihar.gargava@quantinuum.com>
2022-12-15 02:44:02 +0000

    Nihar has commited

--- src/algorithms/exact_synth_hashtable_lookup.rs
+++ src/algorithms/exact_synth_hashtable_lookup.rs
@@ -4,7 +4,9 @@ use std::io::prelude::*;
 use std::io::{BufRead, BufReader};
 
 
-type unsigned_16_bits = u16;
+type UnsignedBits = u16;
+
+type SizeOfBits = u32;
 
 pub fn read_hash_table(filename: &str) -> std::io::Result<HashMap<i32, i32>> {
     let file = File::open(filename)?;
@@ -37,21 +39,20 @@ pub fn save_hash_table(table: &HashMap<i32, i32>, filename: &str) -> std::io::Re
 
 
 
-fn has_repeated_zeroes(n: unsigned_16_bits) -> bool {
+pub fn has_repeated_zeroes(n: UnsignedBits, size: SizeOfBits ) -> bool {
     // Convert the integer to a binary string
-    let binary_str = format!("{:b}", n);
 
-    let n= binary_str.len();
-    for i in 1..n
+    for i in 0..(size-1)
     {
-        if binary_str[i] == 0 && binary_str[i-1] == 0 
+        let mask1 = 1 << i;
+        let mask2 = 1 << i+1;
+        if mask1 & n == 0 && mask2 & n == 0
         {
             return true;
         }
     }
 
     return false;
-
 }
 
 
@@ -60,16 +61,13 @@ pub fn generate_gate_table() {
     let mut gatetable = HashMap::new();
     let file_to_be_saved_at = "data/gates_with_small_t_count.dat";
 
-    let mut i : unsigned_16_bits = 0;
+    let mut i : UnsignedBits = 0;
     while i < 65535
     {   
-
-
 
         gatetable.insert(i as i32 , i as i32);
         i = i+1;
 
     }
-
     save_hash_table(&gatetable, file_to_be_saved_at ).unwrap();
 }
